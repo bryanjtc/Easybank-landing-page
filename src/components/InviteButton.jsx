d691964a7a41da31c8a912ba75cdb95086659f63
Bryan Thomas <bryan.thomas25000@icloud.com>
2021-02-16 13:29:10 -0500

    modificaciones y correciones del navbar

---
+++ src/components/InviteButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles/InviteButton.css";
+
+class InviteButton extends React.Component {
+  render() {
+    return <input type="button" class="invite_button" value="Request Invite" />;
+  }
+}
+
+export default InviteButton;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,73 @@
 import React from "react";
 import "./styles/NavBar.css";
-import MobileNavBar from "./MobileNavBar.jsx";
+import InviteButton from "./InviteButton.jsx";
 import Logo from "../images/logo.svg";
-import IconHamburger from "../images/icon-hamburger.svg";
+import OpenMenuIcon from "../images/icon-hamburger.svg";
+import CloseMenuIcon from "../images/icon-close.svg";
 class NavBar extends React.Component {
-
-    render() { 
-        return(
-            <section class="navbar">
-              <MobileNavBar/>
-              <nav>
-                <div class="easypageimages">
-                  <img src={Logo} alt="Logo" />
-                </div>
-                <label for="toggle"
-                  ><img src={IconHamburger} alt="Hamburger Menu"
-                /></label>
-                <input type="checkbox" id="toggle" />
-        
-                <div class="menu">
-                  <a href="/">Home</a>
-                  <a href="/">About</a>
-                  <a href="/">Contact</a>
-                  <a href="/">Blog</a>
-                  <a href="/">Carrers</a>
-                </div>
-        
-                <input type="button" class="button" value="Request Invite" />
-              </nav>
-            </section>);
+  constructor(props) {
+    super(props);
+    this.state = { opened: false };
+  }
+  render() {
+    if (this.state.opened) {
+      return (
+        <section class="navbar_container">
+          <div class="navbar">
+            <div class="navbar_logo_container">
+              <img class="navbar_logo" src={Logo} alt="Logo" />
+              <img
+                src={CloseMenuIcon}
+                alt="cerrar menu"
+                class="navbar_icon"
+                id="close-menu-icon"
+                onClick={() => this.setState({ opened: false })}
+              />
+            </div>
+            <div class="navbar_menu">
+              <a href="/">Home</a>
+              <a href="/">About</a>
+              <a href="/">Contact</a>
+              <a href="/">Blog</a>
+              <a href="/">Carrers</a>
+            </div>
+            <div class="navbar_invite_button">
+              <InviteButton />
+            </div>
+          </div>
+        </section>
+      );
     }
+
+    return (
+      <section class="navbar_container">
+        <div class="navbar">
+          <div class="navbar_logo_container">
+            <img class="navbar_logo" src={Logo} alt="Logo" />
+            <img
+              src={OpenMenuIcon}
+              alt="abrir menu"
+              class="navbar_icon"
+              id="open-menu-icon"
+              onClick={() => this.setState({ opened: true })}
+            />
+          </div>
+          <div class="navbar_menu_container">
+            <div class="navbar_menu">
+              <a href="/">Home</a>
+              <a href="/">About</a>
+              <a href="/">Contact</a>
+              <a href="/">Blog</a>
+              <a href="/">Carrers</a>
+            </div>
+          </div>
+          <div class="navbar_invite_button">
+            <InviteButton />
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default NavBar;
